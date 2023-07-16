3fb2d9efac295edfd2e6c97064dcb26381770da7
Diego <Diegofernandomartinezcalderon@gmail.com>
2023-07-16 16:32:39 -0500

    feat: :sparkles: avance de glosario
    se logra un avance del resumen hasta la pagina 24 empezando: Diferencias de Apis globales JavaScript en Node

---
+++ glosarioNode.js
@@ -0,0 +1,66 @@
+/** QUÉ ES NODEJS? 
+ * 
+ *  Es un compilador de codigo para JavaScript que utiliza el motor v8 de Google Chrome
+ * este compilador tiene la ventaja de poder ejecutar codigo Js de lado del servidor lo que 
+ * nos permite realizar aplicaciones de servidor y realizar tareas del lado del servidor como 
+ * manipular archivos y bases de datos.
+*/
+
+/**
+ * Utiliza modelo E/S sin bloqueo 
+ * Basado en eventos
+ * maneja muchas solicitudes simultaneamente
+ * ideal para aplicaciones de alto rendimiento
+ * Administrador de paquete Npm (node package manager)
+ */
+
+/**PROCESO DE FUNCIONALIDAD NODE.JS 
+ * 
+ * INICIACION:Cuando se inicia la ejecucion se cargan modulos necesarios
+ * se cargan las variables de entorno 
+ * y los parametros de ejecucion.
+ * 
+ * EVENT LOOP MANEJO DE EVENTOS: El bucle de eventos es el responsable de recibir y distribuir
+ * eventos en el programa  Node.js espera eventos y ejecuta devoluviones 
+ * 
+ * GESTION DE SOLICITUDES:Cuando llega una solictud se desencadena un evento
+ * 
+ * OPERACIONES E/S NO BLOQUEANTES: lo que significa que puede iniciar una operacion como leer o
+ * escribir en una archivo, hacer una solicitud a una base de datos o envar una solicitud HTTP y continuar
+ * con otras tareas  si esperar que se completen las anteriores
+ * 
+ * DEVOLUCIONES DE LLAMADA(CALLBACK): son funciones que se registran para ejecutarse después de que se compete 
+ * una operacion asincronica, esto permite que node maneje multiples solicitudes de operaciones eficientemente.
+ * 
+ * MODULOS Y NPM:Utiliza un sistema de modulos basado en commonJs, lo que permite lamodularidad y la reutilizaciónde codigo
+ * NPM: es un administrador depaquetes que facilita la instalacion, actualizacion y administracion de bibliotecas y modulos de terceros
+*/
+
+/**
+ * QUE ES NVM? (NODE VERSION MANAGER)
+ * permite administrar varias versiones de node(instalar, actualizar, etc.)
+ * 
+ * Comandos basicos de nvm: 
+ * nvm check: verifica el proceso de nvm4w en buscade problemas
+ * nvm current: muestra la verion activa
+ * nvm install(versio..): instala la version de node que quiere
+ * nvm list available: lista de las versiones de node que tiene instaladas
+ * nvm on: habilita node
+ * nvm of: deshabilita el node
+ * nvm uninstall(versio): desinatala la version que quieras
+ * nvm use (version): utiliza la version de node que quieras
+ * nvm version: muestra la version de node que se está usando
+ */
+
+/** THUNDER CLIENT:
+ * 
+ * Es una herramienta  extension de visual que te permite realizar solicitudes HTTP y probar servicios web 
+ * dirtectamente desde el editor de codigo  
+ */
+
+
+/** EJECUTAR  UN ARCHIVO EN NODE.JS
+ * 
+ * Para ejecutar un archivo solo basta con abrir la terminal y escribir el comando node (nombre del archivo que vas a correr)
+ * importante(debe ser un archivo tipo .js)
+ */
